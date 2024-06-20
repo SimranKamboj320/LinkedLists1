280df665f26eff470cb9a9fcb610baf794db9bde
SimranKamboj320 <simrankamboj320@gmail.com>
2024-06-21 01:31:17 +0530

    Reverse a Linked List
    Reverse a Linked List

---
+++ ReverseLL.java
@@ -0,0 +1,67 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void Addfirst(int data){
+        //1 - Create new node
+        Node newNode = new Node(data);
+        size++;
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        //2 - newNode next = Head
+        newNode.next = head; //link
+        //3 - head = newNode
+        head = newNode;
+    }
+    public void print(){
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+">");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+    public void ReverseLL(){
+        Node prev = null;
+        Node curr = tail = head;//assignment right to left
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    public static void main(String args[]){
+        LinkedList ll = new LinkedList();
+        ll.Addfirst(2);
+        ll.Addfirst(1);
+        ll.Addfirst(4);
+        ll.Addfirst(5);
+        ll.Addfirst(7);
+        ll.Addfirst(8);
+
+        ll.print();
+        ll.ReverseLL(); 
+        ll.print();
+    }
+}

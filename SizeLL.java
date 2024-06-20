df828c238fea9ae967f8195ea56d6aabe981e528
SimranKamboj320 <simrankamboj320@gmail.com>
2024-06-20 16:45:21 +0530

    Size of LL
    Size of a Linked List

---
+++ SizeLL.java
@@ -0,0 +1,62 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void Addfirst(int data){
+        //1 - Create new node
+        Node newNode = new Node(data);
+        size++;
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        //2 - newNode next = Head
+        newNode.next = head; //link
+        //3 - head = newNode
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode; //link
+        tail = newNode;
+    }
+    public void print(){
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+">");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+    public static void main(String args[]){
+        LinkedList ll = new LinkedList();
+        ll.Addfirst(2);
+        ll.Addfirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        ll.print(); 
+        System.out.println(ll.size);
+    }
+}
